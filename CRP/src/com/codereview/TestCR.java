e9898009286ff2d8a651d4da10c3ad522d2dcdeb
z003568n <z003568n@md1ztyjc.mshome.net>
2019-02-08 12:33:22 +0530

    CRP-2

---
+++ CRP/src/com/codereview/TestCR.java
@@ -0,0 +1,14 @@
+package com.codereview;
+
+public class TestCR {
+
+	public TestCR() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
